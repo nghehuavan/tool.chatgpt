0b8ebeb6e70960436a9660c9e7d14a543f59b2b6
nghe.huavan <nghe.huavan@gmail.com>
2024-03-02 19:20:39 +0700

    .

--- inject.js
+++ inject.js
@@ -65,19 +65,77 @@ function ensureCodeBlock() {
   return ensureElement('cib-code-block[clipboard-data]');
 }
 
-async function ask(question) {
-  await ensureNoteBook();
-  var intput = await ensureInput();
-  intput.value = question;
-  intput.dispatchEvent(new Event('change', { bubbles: true }));
-
-  var button = await ensureButton();
-  button.click();
-  let loading = await ensureLoading();
-  await ensureLoadingCompleted(loading);
-  var codeEle = querySelectorShadow(document.body, 'cib-code-block[clipboard-data]');
-  return codeEle.getAttribute("clipboard-data")
+function getSections(text) {
+  let lines = text.split('\n');
+  let sections = [];
+  var sectionLine = [];
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    if (line.startsWith('###')) {
+      sections.push(sectionLine.join('\n'));
+      sectionLine = [];
+    } else {
+      sectionLine.push(line);
+    }
+  }
+  if (sectionLine.length > 0) sections.push(sectionLine.join('\n'));
+  return sections;
 }
 
-let result = await ask('get document by id?\n please answer short code block only');
-console.log(result);
+function splitTextIntoParts(text, linesPerPart) {
+  let lines = text.split('\n');
+  let parts = [];
+  for (let i = 0; i < lines.length; i += linesPerPart) {
+    let part = lines.slice(i, i + linesPerPart);
+    parts.push(part.join('\n'));
+  }
+  return parts;
+}
+
+let oldText;
+async function ask() {
+  await ensureNoteBook();
+  let input = await ensureInput();
+  let button = await ensureButton();
+  oldText = input.value;
+  let preText;
+  let longText;
+  let question;
+  let sections = getSections(oldText);
+  if (sections.length == 1) {
+    question = sections[0];
+  } else if (sections.length == 2) {
+    longText = sections[0];
+    question = sections[1];
+  } else if (sections.length == 3) {
+    preText = sections[0];
+    longText = sections[1];
+    question = sections[2];
+  }
+
+  let parts = longText ? splitTextIntoParts(longText, 300) : [question];
+  console.log('split to parts' + parts.length);
+  let outputs = [];
+
+  for (let part of parts) {
+    input.value = preText ?? '' + '\n' + part + '\n' + question ?? '';
+    input.dispatchEvent(new Event('change', { bubbles: true }));
+
+    button.click();
+
+    let loading = await ensureLoading();
+    await ensureLoadingCompleted(loading);
+    let codeEle = querySelectorShadow(document.body, 'cib-code-block[clipboard-data]');
+    let result = codeEle.getAttribute('clipboard-data');
+    outputs.push(result);
+  }
+
+  let codeEle = querySelectorShadow(document.body, 'cib-code-block[clipboard-data]');
+  codeEle.setAttribute('clipboard-data', outputs.join('\n'));
+  input.value = oldText;
+  return outputs;
+}
+
+let result = await ask();
+console.log(result.join('\n'));
+alert('done');
